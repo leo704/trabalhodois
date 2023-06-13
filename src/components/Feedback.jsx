beaecc9c2beafdacc6c35346f05973dbea33feb7
leo704 <84240124+leo704@users.noreply.github.com>
2023-06-13 15:58:30 -0300

    até aqui tudo bem

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -3,27 +3,10 @@ import estrela from "../assets/img/estrela.png";
 import "./Agendehorario.css";
 
 export default function Feedback() {
-  /*
-  <div id="caixaComentario">
-              <p>Nome • Procedimento</p>
-              <p>
-                "
-                <i>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Dolorem natus quibusdam, minima sint vero eveniet sed hic et!
-                  Vitae distinctio deleniti optio nam corporis cum ullam ducimus
-                  quasi dignissimos corrupti.
-                </i>
-                "
-              </p>
-              <p>Nota:</p>
-            </div>
-*/
   const [avaliacao, setAvaliacao] = useState([]);
   const url = "https://apidois-leo704.vercel.app/avaliacao";
 
   useEffect(() => {
-    //console.log("Requisitando json dados da API");
     fetch(url)
       .then((resp) => resp.json())
       .then((resp) => setAvaliacao(resp))
